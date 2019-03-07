970ac15b985013b8126578ab3888d479cf541707
horacio <horacio.estevez@gmail.com>
2019-03-06 21:38:09 -0300

    Agrego programa contador de clicks

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log(mensaje)
 
 let contador = 0
 let body = document.querySelector("body")
+
+body.addEventListener("click",function(){
+	console.log("Se han hecho "+contador+" clicks!");
+	contador = contador+1
+})
